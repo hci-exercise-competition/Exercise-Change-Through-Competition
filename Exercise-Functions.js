63084d960b3656b40d523a03753586ea1d20ebc2
Ikhlas <ikhlas.attarwala@gmail.com>
2019-02-27 22:15:36 -0600

    finished but error-checking units

---
+++ Exercise-Functions.js
@@ -0,0 +1,15 @@
+// GLOBAL VARIABLES:
+
+
+
+
+// FUNCTIONS:
+// Goals_Create.html requires text substitution
+
+function goaltype_pick(clicked_id) {
+    document.getElementById("goals_type").innerHTML = clicked_id;
+}
+
+function goalunit_pick(clicked_metic_unit) {
+    document.getElementById("goals_unit").innerHTML = clicked_metic_unit;
+}
