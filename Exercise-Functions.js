03339e22d8c63cdfb45b3d04567d3a8a41b1ed1f
Ikhlas <ikhlas.attarwala@gmail.com>
2019-02-27 22:53:45 -0600

    final

--- Exercise-Functions.js
+++ Exercise-Functions.js
@@ -11,11 +11,32 @@ function goaltype_pick(clicked_id) {
     document.getElementById("goals_type").innerHTML = clicked_id;
 }
 
-function goalunit_pick(clicked_metic_unit) {
-    // if (document.getElementById("goals_type").innerHTML == "Cycling") {
-    // if (clicked_metric_unit.indexOf(DistList) == True) {
-    document.getElementById("goals_unit").innerHTML = clicked_metic_unit;
-    // }
-    // }
+function goalunit_pick(clicked_metric_unit) {
+    if (document.getElementById("goals_type").innerHTML == "Cycling") {
+        if (DistList.indexOf(clicked_metric_unit) >= 0) {
+            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
+        }
+    }
+    if (document.getElementById("goals_type").innerHTML == "Running") {
+        if (DistList.indexOf(clicked_metric_unit) >= 0) {
+            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
+        }
+    }
+    if (document.getElementById("goals_type").innerHTML == "Weight Lifting") {
+        if (LiftList.indexOf(clicked_metric_unit) >= 0) {
+            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
+        }
+    }
+    if (document.getElementById("goals_type").innerHTML == "Swimming") {
+        if (DistList.indexOf(clicked_metric_unit) >= 0) {
+            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
+        }
+    }
+    if (document.getElementById("goals_type").innerHTML == "Frequency") {
+        if (TimeList.indexOf(clicked_metric_unit) >= 0) {
+            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
+        }
+    }
+
     // && clicked_metric_unit.indexOf(DistList) == True) {
 }
