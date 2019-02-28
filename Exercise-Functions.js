ffc162ea02c611704870a4f98205f8eb1740a6b7
Ikhlas <ikhlas.attarwala@gmail.com>
2019-02-27 22:57:09 -0600

    final2

--- Exercise-Functions.js
+++ Exercise-Functions.js
@@ -12,30 +12,25 @@ function goaltype_pick(clicked_id) {
 }
 
 function goalunit_pick(clicked_metric_unit) {
-    if (document.getElementById("goals_type").innerHTML == "Cycling") {
-        if (DistList.indexOf(clicked_metric_unit) >= 0) {
-            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
-        }
+    if (document.getElementById("goals_type").innerHTML == "Cycling"
+        && DistList.indexOf(clicked_metric_unit) >= 0) {
+        document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
     }
-    if (document.getElementById("goals_type").innerHTML == "Running") {
-        if (DistList.indexOf(clicked_metric_unit) >= 0) {
-            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
-        }
+    if (document.getElementById("goals_type").innerHTML == "Running"
+        && DistList.indexOf(clicked_metric_unit) >= 0) {
+        document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
     }
-    if (document.getElementById("goals_type").innerHTML == "Weight Lifting") {
-        if (LiftList.indexOf(clicked_metric_unit) >= 0) {
-            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
-        }
+    if (document.getElementById("goals_type").innerHTML == "Weight Lifting"
+        && LiftList.indexOf(clicked_metric_unit) >= 0) {
+        document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
     }
-    if (document.getElementById("goals_type").innerHTML == "Swimming") {
-        if (DistList.indexOf(clicked_metric_unit) >= 0) {
-            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
-        }
+    if (document.getElementById("goals_type").innerHTML == "Swimming"
+        && DistList.indexOf(clicked_metric_unit) >= 0) {
+        document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
     }
-    if (document.getElementById("goals_type").innerHTML == "Frequency") {
-        if (TimeList.indexOf(clicked_metric_unit) >= 0) {
-            document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
-        }
+    if (document.getElementById("goals_type").innerHTML == "Frequency"
+        && TimeList.indexOf(clicked_metric_unit) >= 0) {
+        document.getElementById("goals_unit").innerHTML = clicked_metric_unit;
     }
 
     // && clicked_metric_unit.indexOf(DistList) == True) {
